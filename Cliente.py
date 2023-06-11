ab44705ae92022701fa2bb3d50d74a04e38e045a
JPablooo <jartavias@ucenfotec.ac.cr>
2023-06-11 14:37:16 -0600

    Add files

---
+++ Cliente.py
@@ -0,0 +1,9 @@
+class Cliente:
+    def __init__(self, id, nombre, email):
+        self.id = id
+        self.nombre = nombre
+        self.email = email
+        
+    
+C1 = Cliente(123325,"Andrea","ffwfewf")
+
---
+++ IA.py
@@ -0,0 +1,38 @@
+import gradio as gr
+import openai
+import os
+
+openai.api_key = "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"
+
+def openai_process_message(user_message):
+    
+    messages = [
+        {
+            "role": "system",
+            "content": "Act like a personal assistant. You can respond to questions, translate sentences, summarize news, and give recommendations."
+        }
+    ]
+    messages.append({"role": "user", "content": user_message})
+
+    
+    openai_response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages, max_tokens=100)
+
+   
+    response_text = openai_response.choices[0].message.content
+    messages.append({"role": "assistant", "content": response_text})
+    
+    return response_text
+examples = [
+    "Que es un chatbot?",
+    "podrias definir que es una IA?",
+    "dime cual seria una receta para hacer salsa bechamel?",
+]
+demo = gr.Interface(
+    fn=openai_process_message,
+    inputs=gr.Textbox(lines=5, label='Pregunta',placeholder='Escribe tu pregunta'),
+    outputs=gr.Textbox(label='Respuesta'),
+    examples=examples,
+    title="Chatbot OpenAI API",
+)
+if __name__ == "__main__":
+    demo.launch()
