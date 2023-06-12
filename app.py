69fee9835db427e8c494e9d584a0526d20e21f01
JPablooo <jartavias@ucenfotec.ac.cr>
2023-06-11 20:57:05 -0600

    Arreglar error render

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
-from flask import Flask
+from flask import (Flask, render_template)
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
+def index():
    # return "Hello, World!"
    return render_template('index.html')
+
+
+
+
+@app.route("/hola")
+def hola():
+   return "Hello, World!"
+  
