03114b03b268eef1adcf483a725009aa1ece1e00
unknown <artaviajose2103@gmail.com>
2023-07-07 15:58:36 -0600

    Web Update img in home

--- backend/Admin.py
+++ backend/Admin.py
@@ -4,11 +4,9 @@ class Admin(Usuario):
     def __init__(self, id, nombre, email, contrasenna):
         super().__init__(id,nombre,email)
         self.contrasenna = contrasenna
-
     def getContrasenna(self):
         return self.contrasenna
-        
-    
+
 
 
 
